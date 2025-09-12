bff593f8cec5baf292fb8925dabb0c722c415ac6
Trenton Scott <InsaneGriot@Outlook.com>
2025-09-12 16:18:30 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NinerCSEquipmentCheckout
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
